dc7650a209bdfefe9382afa2c3ca53779b039205
Zhang <wenjin@compute1-client-1.ris.wustl.edu>
2022-04-13 00:11:39 -0500

    init

--- server/start.sh
+++ server/start.sh
@@ -2,7 +2,8 @@
 
 
 LSF_DOCKER_PORTS='8008:8008' bsub \
-  -g /d.goldfarb/compute -G compute-d.goldfarb \
+  -g /d.goldfarb/compute \
+  -G compute-d.goldfarb \
   -a 'docker(wenjin27/prosit:test)' \
   -J wenjintest \
   -R 'select[ gpuhost, mem>20000, tmp>20, port8090=1 ] rusage[mem=20000, tmp=20] span[hosts=1]' \
@@ -10,7 +11,7 @@ LSF_DOCKER_PORTS='8008:8008' bsub \
   -n 10 \
   -M 20GB \
   -gpu 'num=1:gmodel=TeslaV100_SXM2_32GB:j_exclusive=yes' \
-  -o /storage1/fs1/d.goldfarb/Active/Projects/Wenjin/code/prosit/xtest.%J.log \
-  -e /storage1/fs1/d.goldfarb/Active/Projects/Wenjin/code/prosit/xtest.%J.err \
+  -o /storage1/fs1/d.goldfarb/Active/Projects/Wenjin/code/hpc-prosit/server/start.%J.log \
+  -e /storage1/fs1/d.goldfarb/Active/Projects/Wenjin/code/hpc-prosit/server/start.%J.err \
   python3 server.py
 
